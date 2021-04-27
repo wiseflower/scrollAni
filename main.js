c83ea048e2ed96468e3c7a227cdf2ef309186a18
Jiyeon <all4me8911@gmail.com>
2021-04-28 07:05:00 +0900

    메인 메시지 스크롤 애니메이션 추가

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
     let = yOffset = 0; //window.pageyOffset 대신 쓸 변수
     let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이값의 합
     let currentScene = 0; //현재 활성화된 (눈 앞에 보고 있는) 씬(scroll-section)
-    
+    let enterNewScene = false; //새로운 scene이 시작된 순간 true;
+
+
     const sceneInfo = [
         {
             //0
@@ -18,7 +20,8 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d'),
             },
             values: {
-                messageA_opacity: [0, 1]
+                messageA_opacity: [0, 1, {start: 0.1, end: 0.2}], //start: , end: 애니메이션 실행되는 키프레임 구간
+                messageA_opacity: [0, 1, {start: 0.3, end: 0.4}],
             }
         },
         {
@@ -71,7 +74,36 @@
     }
 
     function calcValues(values, currentYOffset) {
+        let rv;
+        //현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
         
+
+        //재생구간 분기처리
+        if (values.length === 3) {
+            // start ~ end 사이에 애니메이션 실행
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart;
+            
+            if (currentYOffset >= partScrollStart && currentYOffset<=partScrollEnd){
+                rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+            } else if (currentYOffset < partScrollStart) {
+                rv = values[0];
+            } else if (currentYOffset > partScrollEnd) {
+                rv = values[1];
+            }
+            
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
+
+        //parseInt(); 정수처리
+        //현재 씬의 처음부터 끝까지 재생되는 코드, 분기처리 필요
+        
+        
+        return rv;
     }
 
 
@@ -80,12 +112,14 @@
         const values = sceneInfo[currentScene].values;
         const currentYOffset = yOffset - prevScrollHeight;
 
+        console.log(currentScene);
+
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
-                let messageA_opacity_0 = values.messageA_opacity[0];
-                let messageA_opacity_1 = values.messageA_opacity[1];
-                console.log(calcValues(values.messageA_opacity, currentYOffset));
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+                objs.messageA.style.opacity = messageA_opacity_in;
+                console.log(messageA_opacity_in);
                 break;
             
             case 1:
@@ -104,21 +138,27 @@
 
     
     function scrollLoop() {
+        enterNewScene = false;
+
         prevScrollHeight = 0; //prevScrollHeight값 초기화
         for (let i = 0; i < currentScene; i++){
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
             if (currentScene === 0) return; //브라우저 바운스에서도 씬넘버 마이너스 되는 일을 없애준다.
+            enterNewScene = true;
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
+
+        if (enterNewScene) return;
         
         playAnimation();
 
